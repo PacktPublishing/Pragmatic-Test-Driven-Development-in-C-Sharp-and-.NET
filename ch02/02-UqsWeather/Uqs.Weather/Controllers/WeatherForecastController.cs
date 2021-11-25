c976d399a20e62acd9d5df17c9acff43760101a2
Adam Tibi <AdamTibi@users.noreply.github.com>
2021-11-25 23:07:37 +0000

    Adding the second part of the second chapter

--- ch02/02-UqsWeather/Uqs.Weather/Controllers/WeatherForecastController.cs
+++ ch02/02-UqsWeather/Uqs.Weather/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using AdamTibi.OpenWeather;
 
 namespace Uqs.Weather.Controllers;
 
@@ -6,27 +7,74 @@ namespace Uqs.Weather.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IConfiguration _config;
+
+    private const int FORECAST_DAYS = 5;
+
     private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+{
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild",
+        "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration config)
     {
         _logger = logger;
+        _config = config;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet("GetRealWeatherForecast")]
+    public async Task<IEnumerable<WeatherForecast>> GetReal()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        HttpClient httpClient = new HttpClient();
+        string apiKey = _config["OpenWeather:Key"];
+        Client openWeatherClient = new Client(apiKey, httpClient);
+        const decimal GREENWICH_LAT = 51.4810m;
+        const decimal GREENWICH_LON = 0.0052m;
+        OneCallResponse res = await openWeatherClient
+            .OneCallAsync(GREENWICH_LAT, GREENWICH_LON, new [] {
+                Excludes.Current, Excludes.Minutely,
+                Excludes.Hourly, Excludes.Alerts }, Units.Metric);
+
+        WeatherForecast[] wfs = new WeatherForecast[FORECAST_DAYS];
+        for (int i = 0; i < wfs.Length; i++)
         {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            var wf = wfs[i] = new WeatherForecast();
+            wf.Date = res.Daily[i + 1].Dt;
+            double forecastedTemp = res.Daily[i + 1].Temp.Day;
+            wf.TemperatureC = (int)Math.Round(forecastedTemp);
+            wf.Summary = MapFeelToTemp(wf.TemperatureC);
+        }
+        return wfs;
+    }
+
+    [HttpGet("GetRandomWeatherForecast")]
+    public IEnumerable<WeatherForecast> GetRandom()
+    {
+        WeatherForecast[] wfs = 
+            new WeatherForecast[FORECAST_DAYS];
+        for(int i = 0;i < wfs.Length;i++)
+        {
+            var wf = wfs[i] = new WeatherForecast();
+            wf.Date = DateTime.Now.AddDays(i + 1);
+            wf.TemperatureC = Random.Shared.Next(-20, 55);
+            wf.Summary = MapFeelToTemp(wf.TemperatureC);
+        }
+        return wfs;
+    }
+
+    private string MapFeelToTemp(int temperatureC)
+    {
+        if (temperatureC <= 0)
+        {
+            return Summaries.First();
+        }
+        int summariesIndex = (temperatureC / 5) + 1;
+        if (summariesIndex >= Summaries.Length)
+        {
+            return Summaries.Last();
+        }
+        return Summaries[summariesIndex];
     }
 }
