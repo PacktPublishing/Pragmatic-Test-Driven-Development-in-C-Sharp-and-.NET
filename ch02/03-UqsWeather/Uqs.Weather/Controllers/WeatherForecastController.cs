7dea66ed731ebdcb198ea354ee8609612c1830e0
Adam Tibi <AdamTibi@users.noreply.github.com>
2021-11-28 22:10:33 +0000

    Adding third part

---
+++ ch02/03-UqsWeather/Uqs.Weather/Controllers/WeatherForecastController.cs
@@ -0,0 +1,84 @@
+using Microsoft.AspNetCore.Mvc;
+using AdamTibi.OpenWeather;
+using Uqs.Weather.Wrappers;
+
+namespace Uqs.Weather.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private const int FORECAST_DAYS = 5;
+    private readonly IClient _client;
+    private readonly INowWrapper _nowWrapper;
+    private readonly IRandomWrapper _randomWrapper;
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IConfiguration _config;
+
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild",
+        "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public WeatherForecastController(IClient client, INowWrapper nowWrapper,
+        IRandomWrapper randomWrapper, ILogger<WeatherForecastController> logger, 
+        IConfiguration config)
+    {
+        _client = client;
+        _nowWrapper = nowWrapper;
+        _randomWrapper = randomWrapper;
+        _logger = logger;
+        _config = config;
+    }
+
+    [HttpGet("GetRealWeatherForecast")]
+    public async Task<IEnumerable<WeatherForecast>> GetReal()
+    {
+        const decimal GREENWICH_LAT = 51.4810m;
+        const decimal GREENWICH_LON = 0.0052m;
+        OneCallResponse res = await _client.OneCallAsync
+            (GREENWICH_LAT, GREENWICH_LON, new[] {
+                Excludes.Current, Excludes.Minutely,
+                Excludes.Hourly, Excludes.Alerts }, Units.Metric);
+
+        WeatherForecast[] wfs = new WeatherForecast[FORECAST_DAYS];
+        for (int i = 0; i < wfs.Length; i++)
+        {
+            var wf = wfs[i] = new WeatherForecast();
+            wf.Date = res.Daily[i + 1].Dt;
+            double forecastedTemp = res.Daily[i + 1].Temp.Day;
+            wf.TemperatureC = (int)Math.Round(forecastedTemp);
+            wf.Summary = MapFeelToTemp(wf.TemperatureC);
+        }
+        return wfs;
+    }
+
+    [HttpGet("GetRandomWeatherForecast")]
+    public IEnumerable<WeatherForecast> GetRandom()
+    {
+        WeatherForecast[] wfs = new WeatherForecast[FORECAST_DAYS];
+        for (int i = 0; i < wfs.Length; i++)
+        {
+            var wf = wfs[i] = new WeatherForecast();
+            wf.Date = _nowWrapper.Now.AddDays(i + 1);
+            wf.TemperatureC = _randomWrapper.Next(-20, 55);
+            wf.Summary = MapFeelToTemp(wf.TemperatureC);
+        }
+        return wfs;
+    }
+
+    private string MapFeelToTemp(int temperatureC)
+    {
+        if (temperatureC <= 0)
+        {
+            return Summaries.First();
+        }
+        int summariesIndex = (temperatureC / 5) + 1;
+        if (summariesIndex >= Summaries.Length)
+        {
+            return Summaries.Last();
+        }
+        return Summaries[summariesIndex];
+    }
+}
