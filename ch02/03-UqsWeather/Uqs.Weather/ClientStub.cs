57e2feb1411237738d6b0e56b29169dfe846fb71
Adam Tibi <AdamTibi@users.noreply.github.com>
2021-12-06 23:21:24 +0000

    update

---
+++ ch02/03-UqsWeather/Uqs.Weather/ClientStub.cs
@@ -0,0 +1,23 @@
+﻿using AdamTibi.OpenWeather;
+
+namespace Uqs.Weather;
+
+public class ClientStub : IClient
+{
+    public Task<OneCallResponse> OneCallAsync(
+        decimal latitude, decimal longitude, Excludes[] excludes, Units unit)
+    {
+        const int DAYS = 7;
+        OneCallResponse res = new OneCallResponse();
+        res.Daily = new Daily[DAYS];
+        DateTime now = DateTime.Now;
+        for (int i = 0; i < DAYS; i++)
+        {
+            res.Daily[i] = new Daily();
+            res.Daily[i].Dt = now.AddDays(i);
+            res.Daily[i].Temp = new Temp();
+            res.Daily[i].Temp.Day = Random.Shared.Next(-20, 55);
+        }
+        return Task.FromResult(res);
+    }
+}
--- ch02/03-UqsWeather/Uqs.Weather/Program.cs
+++ ch02/03-UqsWeather/Uqs.Weather/Program.cs
@@ -1,13 +1,19 @@
 using AdamTibi.OpenWeather;
+using Uqs.Weather;
 using Uqs.Weather.Wrappers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<IClient>(_ => {
-    string apiKey = builder.Configuration["OpenWeather:Key"];
-    HttpClient httpClient = new HttpClient();
-    return new Client(apiKey, httpClient);
+    bool isLoad = bool.Parse(builder.Configuration["LoadTest:IsActive"]);
+    if (isLoad) return new ClientStub();
+    else
+    {
+        string apiKey = builder.Configuration["OpenWeather:Key"];
+        HttpClient httpClient = new HttpClient();
+        return new Client(apiKey, httpClient);
+    }
 });
 builder.Services.AddSingleton<INowWrapper>(_ => new NowWrapper());
 builder.Services.AddTransient<IRandomWrapper>(_ => new RandomWrapper());
