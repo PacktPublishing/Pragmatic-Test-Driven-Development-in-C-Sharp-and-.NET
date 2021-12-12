526359ac958890dcbfdd766b3a20761ca38453a4
Adam Tibi <AdamTibi@users.noreply.github.com>
2021-12-12 16:36:25 +0000

    fixing compilation

--- ch02/03-UqsWeather/Uqs.Weather.TestRunner/Program.cs
+++ ch02/03-UqsWeather/Uqs.Weather.TestRunner/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.Extensions.Logging.Abstractions;
 using Uqs.Weather.Controllers;
-var logger = new NullLogger<WeatherForecastController>();
-var controller = new WeatherForecastController(logger, null!, null!, null!, null!);
+var logger = NullLogger<WeatherForecastController>.Instance;
+var controller = new WeatherForecastController(logger, null!, null!, null!);
 double f1 = controller.ConvertCToF(-1.0);
 if (f1 != 30.20d) throw new Exception("Invalid conversion");
 double f2 = controller.ConvertCToF(1.2);
