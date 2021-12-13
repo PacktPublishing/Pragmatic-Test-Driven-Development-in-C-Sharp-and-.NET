13d1fec9e407561fc160bab095e2e2beada3a36d
Adam Tibi <AdamTibi@users.noreply.github.com>
2021-12-13 20:38:16 +0000

    Chapter 3 first commit

---
+++ ch03/Uqs.Weather/Wrappers/RandomWrapper.cs
@@ -0,0 +1,11 @@
+﻿namespace Uqs.Weather.Wrappers;
+
+public class RandomWrapper : IRandomWrapper
+{
+    private readonly Random _random = Random.Shared;
+
+    public int Next(int minValue, int maxValue)
+    {
+        return _random.Next(minValue, maxValue);
+    }
+}
