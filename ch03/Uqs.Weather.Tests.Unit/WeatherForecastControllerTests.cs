c536cc896841e09ac124b4c28df3fbc68e11b5a4
Adam Tibi <AdamTibi@users.noreply.github.com>
2021-12-19 11:18:11 +0000

    Backup

---
+++ ch03/Uqs.Weather.Tests.Unit/WeatherForecastControllerTests.cs
@@ -0,0 +1,35 @@
+using Microsoft.Extensions.Logging.Abstractions;
+using Uqs.Weather.Controllers;
+using Xunit;
+
+namespace Uqs.Weather.Tests.Unit;
+
+public class WeatherForecastControllerTests
+{
+    [Fact]
+    public void ConvertCToF_0Celsius_32Fahrenheit()
+    {
+        const double expected = 32d;
+        var logger = NullLogger<WeatherForecastController>.Instance;
+        var controller = new WeatherForecastController(logger, null!, null!, null!);
+
+        double actual = controller.ConvertCToF(0);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(-100, -148)]
+    [InlineData(-10.1, 13.8)]
+    [InlineData(10, 50)]
+
+    public void ConvertCToF_Celcius_CorrectFahrenheit(double c, double f)
+    {
+        var logger = NullLogger<WeatherForecastController>.Instance;
+        var controller = new WeatherForecastController(logger, 
+            null!, null!, null!);
+        double actual = controller.ConvertCToF(c);
+
+        Assert.Equal(f, actual, 1);
+    }
+}
