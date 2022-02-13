9d480422f4344076ec62ba6212a9feae241a853a
Adam Tibi <AdamTibi@users.noreply.github.com>
2022-02-13 22:54:36 +0000

    SOLID

---
+++ ch05/Samples/NoSolid/Calculations.cs
@@ -0,0 +1,23 @@
+﻿public interface IArithmeticOperation {}
+
+public class Addition : IArithmeticOperation
+{
+    public double Add(double left, double right) => left + right;
+}
+
+public class Subtraction : IArithmeticOperation
+{
+    public double Subtract(double left, double right) => left - right;
+}
+
+public class Calculation
+{
+    public double Calculate(IArithmeticOperation op, double left, double right) => 
+        op switch
+        {
+            Addition addition => addition.Add(left, right),
+            Subtraction subtraction => subtraction.Subtract(left, right),
+            // Multiplication multiplication => multiplication.Multiply(left, right),
+            _ => throw new NotImplementedException()
+        };
+}
---
+++ ch05/Samples/WithSolid/Birds.cs
@@ -0,0 +1,18 @@
+﻿public abstract class Bird
+{
+    public abstract void Fly();
+    public abstract void Walk();
+}
+
+public class Robin : Bird
+{
+    public override void Fly() => Console.WriteLine("fly");
+    public override void Walk() => Console.WriteLine("walk");
+}
+
+public class Ostrich : Bird 
+{
+    public override void Fly() => throw new InvalidOperationException();
+    public override void Walk() => Console.WriteLine("walk");
+}
+
