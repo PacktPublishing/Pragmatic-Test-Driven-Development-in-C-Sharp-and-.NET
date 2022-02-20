fbf869781a45d75a67a931bcbe69f413372054b9
Adam Tibi <AdamTibi@users.noreply.github.com>
2022-02-20 21:40:44 +0000

    Adding chapter 5

---
+++ ch05/TddByExample/Uqs.Customer.Tests.Unit/ProfileServiceTests.cs
@@ -0,0 +1,74 @@
+namespace Uqs.Customer.Tests.Unit;
+
+public class ProfileServiceTests
+{
+    [Fact]
+    public void ChangeUsername_NullUsername_ArgumentNullException()
+    {
+        // Arrange
+        var sut = new ProfileService();
+
+        // Act
+        var e = Record.Exception(() => sut.ChangeUsername(null!));
+
+        // Assert
+        var ex = Assert.IsType<ArgumentNullException>(e);
+        Assert.Equal("username", ex.ParamName);
+        Assert.StartsWith("Null", ex.Message);
+    }
+
+
+    [Theory]
+    [InlineData("ANameOf8", true)]
+    [InlineData("NameOfChar12", true)]
+    [InlineData("ANameOfChar13", false)]
+    [InlineData("NameOf7", false)]
+    [InlineData("", false)]
+    public void ChangeUsername_VariousLengthUsernames_ArgumentOutOfRangeExceptionIfInvalid(string username, bool isValid)
+    {
+        // Arrange
+        var sut = new ProfileService();
+
+        // Act
+        var e = Record.Exception(() => sut.ChangeUsername(username));
+
+        // Assert
+        if (isValid)
+        {
+            Assert.Null(e);
+        }
+        else
+        {
+            var ex = Assert.IsType<ArgumentOutOfRangeException>(e);
+            Assert.Equal("username", ex.ParamName);
+            Assert.StartsWith("Length", ex.Message);
+        }
+    }
+
+    [Theory]
+    [InlineData("Letter_123", true)]
+    [InlineData("!The_Start", false)]
+    [InlineData("InThe@Middle", false)]
+    [InlineData("WithDollar$", false)]
+    [InlineData("Space 123", false)]
+    public void ChangeUsername_InvalidCharValidation_ArgumentOutOfRangeException(string username, bool isValid)
+    {
+        // Arrange
+        var sut = new ProfileService();
+
+        // Act
+        var e = Record.Exception(() => sut.ChangeUsername(username));
+
+        // Assert
+        if (isValid)
+        {
+            Assert.Null(e);
+        }
+        else
+        {
+            var ex = Assert.IsType<ArgumentOutOfRangeException>(e);
+            Assert.Equal("username", ex.ParamName);
+            Assert.StartsWith("InvalidChar", ex.Message);
+        }
+    }
+}
---
+++ ch05/TddByExample/Uqs.Customer/ProfileService.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Uqs.Customer;
+
+public class ProfileService
+{
+    private const string ALPHANUMERIC_UNDERSCORE_REGEX = @"^[a-zA-Z0-9_]+$";
+    private static readonly Regex _formatRegex = new (ALPHANUMERIC_UNDERSCORE_REGEX, RegexOptions.Compiled);
+    public void ChangeUsername(string username)
+    {
+        if (username == null)
+        {
+            throw new ArgumentNullException(nameof(username), "Null");
+        }
+        if (username.Length is < 8 or > 12) 
+        {
+            throw new ArgumentOutOfRangeException(nameof(username), "Length");
+        }
+        if (!_formatRegex.Match(username).Success)
+        {
+            throw new ArgumentOutOfRangeException(nameof(username), "InvalidChar");
+        }
+    }
+}
