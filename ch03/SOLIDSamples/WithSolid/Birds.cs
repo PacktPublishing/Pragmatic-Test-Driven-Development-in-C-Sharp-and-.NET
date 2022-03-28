428932e22e6456ca0f055a3d2e9bc753128096cf
Adam Tibi <AdamTibi@users.noreply.github.com>
2022-03-28 18:27:27 +0100

    Shuffling directories

---
+++ ch03/SOLIDSamples/WithSolid/Birds.cs
@@ -0,0 +1,20 @@
+﻿public abstract class Bird
+{
+    public abstract void Walk();
+}
+public abstract class FlyingBird : Bird
+{
+    public abstract void Fly();
+}
+
+public class Robin : FlyingBird
+{
+    public override void Fly() => Console.WriteLine("fly");
+    public override void Walk() => Console.WriteLine("walk");
+}
+
+public class Ostrich : Bird 
+{
+    public override void Walk() => Console.WriteLine("walk");
+}
+
---
+++ ch03/SOLIDSamples/WithSolid/Calculations.cs
@@ -0,0 +1,26 @@
+﻿public interface IArithmeticOperation 
+{
+    public double Operate(double left, double right);
+}
+
+public class Addition : IArithmeticOperation
+{
+    public double Operate(double left, double right) => left + right;
+}
+
+public class Subtraction : IArithmeticOperation
+{
+    public double Operate(double left, double right) => left - right;
+}
+
+public class Multiplication : IArithmeticOperation
+{
+    public double Operate(double left, double right) => left * right;
+}
+
+public class Calculation
+{
+    public double Calculate(IArithmeticOperation op, double left, double right) =>
+        op.Operate(left, right);
+        
+}
